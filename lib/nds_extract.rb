7a0495d7098dcb19793ef367eb65cd8d18f175da
Valerie Foster <fosterv2@outlook.com>
2020-01-14 01:28:20 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,7 +46,7 @@ def movies_with_director_key(name, movies_collection)
   # by using the provided movie_with_director_name method
   movie_array = []
   movie_index = 0
-  while movie_index < movies_collection.legth do
+  while movie_index < movies_collection.length do
     movie_array << movie_with_director_name(name, movies_collection[movie_index])
     movie_index += 1
   end
